c185113aad1cc02a0f51196e4932be2d646eb76e
Hyplast <sseveri@gmail.com>
2022-11-28 17:13:03 +0200

    Testing version working

--- timelapse.py
+++ timelapse.py
@@ -1,73 +1,21 @@
-import PyPDF2
+# import module
 import os
-from PIL import Image
-from PyPDF2 import PdfReader
+import tempfile
+from pdf2image import convert_from_path
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
-# if os.path.exists('C:\\Users\\sseve\\Pictures\\20221128-full-color-ice-chart.pdf'):
-#   print("The file exist")
-# else:
-#   print("The file does not exist")
-
-print("Select starting date: ")
-
-# reader = PdfReader("example.pdf")
-reader = PdfReader('C:\\Users\\sseve\\Pictures\\New folder\\20221128-full-color-ice-chart.pdf')
-
-page = reader.pages[0]
-print("Select ending date: ")
-
-count = 0
-
-# with open('C:\\Users\\sseve\\Pictures\\New folder\\20221128-full-color-ice-chart.pdf', 'rb') as pdf_file:
-#     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-#     print(f'Number of Pages in PDF File is {pdf_reader.getNumPages()}')
-
-for image_file_object in page.images:
-    with open(str(count) + image_file_object.name, "wb") as fp:
-        fp.write(image_file_object.data)
-        count += 1
-
-print(page.extract_text(0))
-print(count)
-
-    #     # extracting images from the 1st page
-    # page0 = pdf_reader.getPage(0)
-
-    # if '/XObject' in page0['/Resources']:
-    #     xObject = page0['/Resources']['/XObject'].getObject()
-
-    #     for obj in xObject:
-    #         if xObject[obj]['/Subtype'] == '/Image':
-    #             size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-    #             data = xObject[obj].getData()
-    #             if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-    #                 mode = "RGB"
-    #             else:
-    #                 mode = "P"
-
-    #             if '/Filter' in xObject[obj]:
-    #                 if xObject[obj]['/Filter'] == '/FlateDecode':
-    #                     img = Image.frombytes(mode, size, data)
-    #                     img.save(obj[1:] + ".png")
-    #                 elif xObject[obj]['/Filter'] == '/DCTDecode':
-    #                     img = open(obj[1:] + ".jpg", "wb")
-    #                     img.write(data)
-    #                     img.close()
-    #                 elif xObject[obj]['/Filter'] == '/JPXDecode':
-    #                     img = open(obj[1:] + ".jp2", "wb")
-    #                     img.write(data)
-    #                     img.close()
-    #                 elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
-    #                     img = open(obj[1:] + ".tiff", "wb")
-    #                     img.write(data)
-    #                     img.close()
-    #             else:
-    #                 img = Image.frombytes(mode, size, data)
-    #                 img.save(obj[1:] + ".png")
-    # else:
-    #     print("No image found.")
-    
-    # # print(f'PDF Metadata is {pdf_reader.documentInfo}')
-    # # print(f'PDF File Author is {pdf_reader.documentInfo["/Author"]}')
-    # # print(f'PDF File Creator is {pdf_reader.documentInfo["/Creator"]}')
-
+path="/home/safka/timelapse-sea-ice-fmi/"
+output_folder_path="/home/safka/timelapse-sea-ice-fmi/output/"
+pdfname=path+"peedeeaff.pdf"
+inputpdf = PdfFileReader(open(pdfname,"rb"))
+maxPages = inputpdf.numPages
+print("Number of pages in PDF="+str(maxPages))
+i = 1
+for page in range(1, maxPages, 10):
+    pil_images = convert_from_path(pdfname, dpi=200, first_page=page,
+                                                     last_page=min(page + 10 - 1, maxPages), fmt= 'jpg',
+                                                     thread_count=1, userpw=None,
+                                                     use_cropbox=False, strict=False)
+    for image in pil_images:
+        image.save(output_folder_path+'output' + str(i) + '.jpg', 'JPEG')
+        i = i + 1
